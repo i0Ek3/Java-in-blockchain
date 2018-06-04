35aa734865b5afdaf354fdccd084793114f25637
i0Ek3 <kno30826@gmail.com>
2018-06-04 21:20:00 +0800

    Finished the part of transaction

---
+++ Part2/Javachain/TransactionInput.java
@@ -0,0 +1,16 @@
+// 
+// TransactionInput.java
+// @ianpasm(kno30826@gmail.com)
+// 2018-06-04 21:10:16
+// 
+ 
+package javachain;
+
+public class TransactionInput {
+    public String transactionOutputId;
+    public TransactionOutput UTXO;
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+}
---
+++ Part2/Javachain/TransactionOutput.java
@@ -0,0 +1,26 @@
+// 
+// TransactionOutput.java
+// @ianpasm(kno30826@gmail.com)
+// 2018-06-04 21:13:14
+// 
+ 
+package javachain;
+import java.security.PublicKey;
+
+public class TransactionOutput {
+    public String id;
+    public PublicKey reciepient; //marked new owner
+    public float value;
+    public String parentTransactionId;
+
+    public TransactionOutput(PublicKey reciepient, float value, String parentTransactionId) {
+        this.reciepient = reciepient;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        this.id = id;
+    }
+
+    public boolean isMined(PublicKey publicKey) {
+        return (publicKey == reciepient);
+    }
+}
